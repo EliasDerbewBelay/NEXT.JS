00a9a8dbb1eabc153515e29bb4de827e83bdcdfe
eliasderbewbelay <eliasderbew1@gmail.com>
2026-03-01 03:18:17 +0300

    Feat/Light rays setup for the layout page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <div className="text-4xl underline">This is Next.js Course</div>;
+  return (
+    <section className="mt-9">
+      <h1 className="text-center text-4xl font-bold">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackthons, Meetups, and Conferences, All in one place{" "}
+      </p>
+    </section>
+  );
 };
 
 export default Home;
